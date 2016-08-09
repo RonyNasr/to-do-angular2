262d4e5036454444f94126c1e6f57154a8540b94
Matt Wilkin and Rony Nasr <rony@>
2016-08-09 14:58:20 -0700

    add priority filter.

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -4,20 +4,27 @@ import { Task } from './task.model';
 import { EditTaskDetailsComponent } from './edit-task-details.component';
 import { NewTaskComponent } from './new-task.component';
 import {DonePipe} from './done.pipe';
+import {PriorityPipe} from './priority.pipe';
 
 @Component({
   selector: 'task-list',
   inputs: ['taskList'],
   outputs: ['onTaskSelect'],
-  pipes: [DonePipe],
+  pipes: [DonePipe, PriorityPipe],
   directives: [TaskComponent, EditTaskDetailsComponent, NewTaskComponent],
   template: `
-  <select (change)="onChange($event.target.value)" class="filter">
+  <select (change)="onChange($event.target.value)" id="filter">
     <option value="all">Show All</option>
     <option value="done">Show Done</option>
     <option value="notDone" selected="selected">Show Not Done</option>
   </select>
-  <task-display *ngFor="#currentTask of taskList | done:filterDone"
+  <select (change)="onPriorityChange($event.target.value)" id="filter">
+    <option value="All" selected="selected">Show All</option>
+    <option value="High">Show High</option>
+    <option value="Medium">Show Medium</option>
+    <option value="Low">Show Low</option>
+  </select>
+  <task-display *ngFor="#currentTask of taskList | done:filterDone | priority:filterPriority"
     (click)="taskClicked(currentTask)"
     [class.selected]="currentTask === selectedTask"
     [task]="currentTask">
@@ -32,6 +39,7 @@ export class TaskListComponent {
   public onTaskSelect: EventEmitter<Task>;
   public selectedTask: Task;
   public filterDone: string = "notDone";
+  public filterPriority: string = "All";
   constructor() {
     this.onTaskSelect = new EventEmitter();
   }
@@ -44,7 +52,14 @@ export class TaskListComponent {
       new Task(description, this.taskList.length, priority, category)
     );
   }
-  onChange(filterOption) {
-    this.filterDone = filterOption;
+  onChange(doneOption) {
+    this.filterDone = doneOption;
+    console.log(this.filterPriority);
+
   }
+  onPriorityChange(priorityOption) {
+    this.filterPriority = priorityOption;
+    console.log(this.filterDone);
+  }
+
 }
